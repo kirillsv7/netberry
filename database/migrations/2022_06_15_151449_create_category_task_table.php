9f69700beda42d0f1f331b2881324c5a00b42e06
Kirill <3025889+kirillsv7@users.noreply.github.com>
2022-06-15 18:29:36 +0200

    Task, Category models and migrations

---
+++ database/migrations/2022_06_15_151449_create_category_task_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_task', function (Blueprint $table) {
+            $table->foreignId('category_id')
+                  ->constrained()
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+            $table->timestamps();
+            $table->foreignId('task_id')
+                  ->constrained()
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_task');
+    }
+};
